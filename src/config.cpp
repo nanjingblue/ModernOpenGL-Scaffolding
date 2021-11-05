24616220f3b402d7d6704b147a5c8f4dbca3cf5c
nanjingblue <mayitrust@gmail.com>
2021-11-05 09:20:05 +0800

    update src

---
+++ src/config.cpp
@@ -0,0 +1,6 @@
+//
+// Created by cherry on 2021/11/5.
+//
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-#include <vector>
 #include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
+#include "config.cpp"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+GLFWwindow* initGlfw();
+void initGlad();
+unsigned int initOpenGL(GLFWwindow *window);
+unsigned int makeVAO(float vertices[]);
+void draw(GLFWwindow * window, unsigned int program, unsigned int VAO);
 
 // 暂时将顶点着色器的源代码硬编码在代码文件顶部的C风格字符串中
 const char *vertexShaderSource = "#version 330 core\n"
@@ -22,11 +30,40 @@ const char *fragmentShaderSource = "#version 330 core\n"
 
 // 定义顶点数组
 float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
 };
-const unsigned int size = sizeof(vertices);
+const unsigned int verticesSize = sizeof(vertices);
+
+unsigned int indices[] = { // 注意索引从0开始!
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+};
+const unsigned int indicesSize = sizeof(indices);
+
+int main() {
+    GLFWwindow *window = initGlfw();
+    initGlad();
+    unsigned int shaderProgram = initOpenGL(window);
+
+    // 创建顶点数组对象
+    unsigned int VAO = makeVAO(vertices);
+
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);  // 框线模式
+
+    // 渲染循环
+    while(!glfwWindowShouldClose(window)) {
+        // 键盘输入
+        processInput(window);
+        // 渲染
+        draw(window, shaderProgram, VAO);
+    }
+    glDeleteProgram(shaderProgram);
+    glfwTerminate();        // 渲染循环结束后我们需要正确释放/删除之前的分配的所有资源
+    return 0;
+}
 
 /*
  * framebuffer_size_callback
@@ -53,8 +90,8 @@ GLFWwindow* initGlfw() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);      // MAC OS
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL) {
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         exit(-1);
@@ -72,7 +109,7 @@ void initGlad() {
 }
 
 unsigned int initOpenGL(GLFWwindow *window) {
-    glViewport(0, 0, 800, 600); // OpenGL渲染窗口的尺寸大小
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT); // OpenGL渲染窗口的尺寸大小
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     // // 创建着色器对象
     unsigned int vertextShader;
@@ -99,50 +136,41 @@ unsigned int initOpenGL(GLFWwindow *window) {
 
 // makeVao 执行初始化并从提供的点里面返回一个顶点数组
 unsigned int makeVAO(float vertices[]) {
-    // 生成 VBO 对象
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);  // 赋予一个缓冲 ID
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);     // 把新创建的缓冲绑定到 顶点缓冲对象的缓冲类型 GL_ARRAY_BUFFER目标上
-    glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
-
-    // 创建 VAO 对象
-    unsigned int VAO;
+    // 创建 VAO VBO EBO 对象
+    unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);     // 绑定和配置对应的VBO和属性指针
-    glBindVertexArray(VAO);
+    glGenBuffers(1, &VBO);  // 赋予一个缓冲 ID
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);     // 绑定一个顶点数组对象
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);     // 把新创建的缓冲绑定到 顶点缓冲对象的缓冲类型 GL_ARRAY_BUFFER目标上
+    glBufferData(GL_ARRAY_BUFFER, verticesSize, vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesSize, indices, GL_STATIC_DRAW);
 
     // OpenGL该如何解析顶点数据
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
     glEnableVertexAttribArray(0);
 
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     return VAO;
 }
 
-int main() {
+void draw(GLFWwindow * window, unsigned int program, unsigned int VAO) {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);       // 自定义的颜色清空屏幕
+    glClear(GL_COLOR_BUFFER_BIT);       // 使用颜色缓冲位
 
-    GLFWwindow* window = initGlfw();
-    initGlad();
-    unsigned int shaderProgram = initOpenGL(window);
+    // 激活着色器程序对象
+    glUseProgram(program);
+    glBindVertexArray(VAO);
+//        glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-    // 创建顶点数组对象
-    unsigned int VAO = makeVAO(vertices);
-
-    // 渲染循环
-    while(!glfwWindowShouldClose(window)) {
-        // 键盘输入
-        processInput(window);
-        // 渲染
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);       // 自定义的颜色清空屏幕
-        glClear(GL_COLOR_BUFFER_BIT);       // 使用颜色缓冲位
-
-        // 激活着色器程序对象
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        // 检查并调用事件，交换缓冲
-        glfwSwapBuffers(window);        // 交换缓冲
-        glfwPollEvents();   // 查有没有触发什么事件
-    }
-    glfwTerminate();        // 渲染循环结束后我们需要正确释放/删除之前的分配的所有资源
-    return 0;
+    // 检查并调用事件，交换缓冲
+    glfwSwapBuffers(window);        // 交换缓冲
+    glfwPollEvents();   // 查有没有触发什么事件
 }
